db5143785fce2207235355efaa09ea8eaa77de79
She-ilah <sheilahmutale1@gmail.com>
2023-10-31 03:01:44 -0700

    advanced tasks

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Locked class is defined"""
+
+
+class LockedClass:
+    """
+    Prevents user from creating new LockedClass
+    attributes for anything but attributes called
+    'first_name'.
+    """
+
+
+    __slots__ = ["first_name"]
