6de28b9364993b54a56fa3369a6f125c8dd7fba0
She-ilah <sheilahmutale1@gmail.com>
2023-11-06 01:37:41 -0800

    advanced task 2

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Writes a class (MyInt) that inherits from int"""
+
+
+class MyInt(int):
+    """Inverts int operators == and !="""
+
+    def __eq__(self, value):
+        """Make == operator imitate != operator"""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Make != operator imitate == operator"""
+        return self.real == value
