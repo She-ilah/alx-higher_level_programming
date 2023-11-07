b6f29c76f69d48117c12def1d813c62dca356f95
She-ilah <sheilahmutale1@gmail.com>
2023-11-07 01:15:28 -0800

    advanced task 1

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Function  inserts a line of text to a file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Appends after each line containing a specific string"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        line_list = []
+        while True:
+            line = f.readline()
+            if line == "":
+                break
+            line_list.append(line)
+            if search_string in line:
+                line_list.append(new_string)
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.writelines(line_list)
